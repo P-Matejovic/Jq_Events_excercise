716af34338c982f16c894398cfa67e164e5c55a5
P-Matejovic <107559716+P-Matejovic@users.noreply.github.com>
2022-06-18 19:02:46 +0200

    Done
    After few hours i kill the input , but text-to speach win, so i chose alert !

--- javaScript.js
+++ javaScript.js
@@ -6,6 +6,22 @@ $(".btn2").on("click", function() {
     $("p").html("<p>change<p>")
 });
 
+
 $(".btn3").on("click", function() {
-    $("button").css("background-color","red")
+    $("button").css("background-color", "red")
+
+});
+
+function changingColorrrrr () {
+    let color = document.getElementById("colorInpuText").value;  //Takto dostanu hodnotu z toho debilního inputu !
+    $("button").css("background-color", color);
+};
+$("#Changer").click(changingColorrrrr);
+
+
+
+$("#Changer").click(function(
+){
+    let Text = document.getElementById("colorInpuText").value;
+    alert(Text);
 });
